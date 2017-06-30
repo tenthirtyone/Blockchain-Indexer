f185c64b7a16fb1e5ed830a4494fead8280a4861
tenthirtyone <Spactigus@gmail.com>
2017-06-29 20:41:15 -0400

    Tokenizer wired up to express. Refresh browser to see next token

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const txs = require('./txs');
+let idx = 0;
+
+const tokens = txs.map(tx => {
+  return {
+    inputs: tx.inputs.map(input => {
+      if (input.prev_out) {
+        return input.prev_out.addr + ':' + input.prev_out.value
+      } else {
+        return null;
+      }
+    }),
+    outputs: tx.out.map(output => {
+      return output.addr + ':' + output.value;
+    })
+  }
+});
 
 app.get('/', (req, res) => {
-  res.send(true)
-})
+  res.send(tokens[idx]);
+  idx++;
+});
 
 app.listen(3000, () => {
-  console.log('listnin on port 3000')
-})
+  console.log('listnin on port 3000');
+});
