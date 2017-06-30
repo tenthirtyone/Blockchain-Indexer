ef8b0cf604715c4539665724035f95df14fe9774
tenthirtyone <Spactigus@gmail.com>
2017-06-30 00:03:55 -0400

    Reducer finished. Current sample size results in negative values.

--- app.js
+++ app.js
@@ -6,22 +6,35 @@ const mapper = express();
 
 let idx = 0;
 
+let addressMap = {};
+let addressMapKeys = [];
+let addressMapIdx = 0;
+
 const tokens = txs.map(tx => {
   return [
     tx.inputs.map(input => {
       if (input.prev_out) {
-        return input.prev_out.addr + ':' + -input.prev_out.value
+        return {
+          addr: input.prev_out.addr,
+          val: -input.prev_out.value,
+        }
       } else {
+        // implicit but adding for clarity
         return null;
       }
     }),
     tx.out.map(output => {
-      return output.addr + ':' + output.value;
+      return {
+        addr: output.addr,
+        val: output.value,
+      }
     })
   ].reduce((a, b) => {
     return a.concat(b);
   });
-});
+}).reduce((a, b) => {
+    return a.concat(b);
+  });
 
 tokenizer.get('/', (req, res) => {
   if (idx < tokens.length - 1) {
@@ -33,19 +46,77 @@ tokenizer.get('/', (req, res) => {
 });
 
 tokenizer.listen(3000, () => {
-  console.log('listnin on port 3000');
+  console.log('Tokenizer listening on port 3000');
+});
+
+
+mapper.get('/', (req, res) => {
+  if (addressMapIdx < addressMapKeys.length - 1) {
+    let addr = addressMapKeys[addressMapIdx];
+    res.send({
+      addr: addr,
+      vals: addressMap[addr],
+    });
+    addressMapIdx++;
+  } else {
+    res.status(204).send();
+  }
+});
+
+mapper.listen(3001, () => {
+  console.log('Mapper listening on port 3001');
 });
 
 mapToken();
 
 function mapToken() {
   setTimeout(() => {
-    request('http://localhost:3000', (err, res, body) => {
+    request('http://localhost:3000', (err, res, token) => {
+
       if (res.statusCode === 200) {
-        console.log(body);
+        try {
+          token = JSON.parse(token);
+        } catch(e) {
+          console.log(e)
+          token = null;
+        }
+        if (token) {
+          if (addressMap[token.addr]) {
+            addressMap[token.addr].push(token.val);
+          } else {
+            addressMap[token.addr] = [token.val];
+            addressMapKeys.push(token.addr);
+          }
+        }
         mapToken();
       } else {
         console.log('mapping complete');
+        reduceAddresses();
+      }
+    })
+  });
+}
+
+function reduceAddresses() {
+  setTimeout(() => {
+    request('http://localhost:3001', (err, res, address) => {
+
+      if (res.statusCode === 200) {
+        try {
+          address = JSON.parse(address);
+        } catch(e) {
+          console.log(e)
+          address = null;
+        }
+        if (address) {
+          address.vals = address.vals.reduce((a, b) => {
+            return a+b;
+          })
+          console.log(address);
+        }
+        reduceAddresses();
+      } else {
+        console.log('reduce complete');
       }
     })
   });
